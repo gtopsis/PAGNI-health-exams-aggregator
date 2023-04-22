e55eb544817fdbd9f16cc070c8fe5c6f8efc64d8
gtopsis <csd3004@csd.uoc.gr>
2023-04-22 10:46:39 +0300

    increase the accuracy of query tokens

--- src/fileSearcher.ts
+++ src/fileSearcher.ts
@@ -1,21 +1,21 @@
 const healthMetrics = [
-  "WBC",
-  "Ne",
-  "Ly",
-  "Mo",
-  "Eos",
-  "Bas",
-  "RBC",
-  "HGB",
-  "HCT",
-  "MCV",
-  "MCH",
-  "MCHC",
-  "RDW",
-  "PLT",
-  "MPV",
-  "PCT",
-  "PDW",
+  "WBC  Λευκά αιμοσφ.",
+  "Ne Ουδετερόφιλα",
+  "Ly Λεμφοκύτταρα",
+  "Mo Μονοκύτταρα",
+  "Eos Ηωσινόφιλα",
+  "Bas Βασεόφιλα",
+  "RBC Ερυθρά αιμοσφαίρια",
+  "HGB Αιμοσφαιρίνη",
+  "HCT Αιματοκρίτης",
+  "MCV Μέσος όγκος",
+  "MCH Μέση περ.Hb",
+  "MCHC Μέση πυκνότητα",
+  "RDW Εύρος καταν.ερυθρ.",
+  "PLT Αιμοπετάλια",
+  "MPV Μέσος όγκος αιμοπεταλίων",
+  "PCT Αιμοπεταλιοκρίτης",
+  "PDW Εύρος κατανομής",
 ] as const;
 
 type TokensType = (typeof healthMetrics)[number];
@@ -31,7 +31,7 @@ export const textSearch = (contents: string): FileResults["result"] => {
   const lines = contents.toString().split("\n");
   lines.forEach((line) => {
     healthMetrics.forEach((token) => {
-      const regEx = new RegExp(".*[0-9].*" + token + " ", "i");
+      const regEx = new RegExp("[0-9].*" + token, "i");
 
       if (line && line.search(regEx) >= 0) {
         console.log(line);
