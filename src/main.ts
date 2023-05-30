c76d1573c36f11e2c7755531edc37dca32e3c80f
gtopsis <csd3004@csd.uoc.gr>
2023-05-30 17:37:03 +0300

    Adds lib vuetify and its relative vite's plugin

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-// import './samples/node-api'
 
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 createApp(App)
+  .use(vuetify)
   .mount("#app")
   .$nextTick(() => {
     postMessage({ payload: "removeLoading" }, "*");
